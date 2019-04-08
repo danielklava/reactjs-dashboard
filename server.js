b51c33aeeb30e57e43ea835c9afc56ce8278707e
Daniel Klava <klava.daniel@gmail.com>
2019-04-08 14:46:42 -0300

    heroku build

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(process.env.PORT, '0.0.0.0', function() {
   console.log('listening on port ', server.address().port);
 });
